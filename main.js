95e492c7cb4b933b640f5beaa1b81108fcf79b0d
Laurent Meyer <laurent.m.meyer@gmail.com>
2020-10-10 21:46:17 +0200

    Adapted to the new version of ebay.de

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
 // This is the content script
 
-const name = $("#buyercontactname").val();
-const address1 = $('#buyeraddress1').val();
-const address2 = $('#buyeraddress2').val();
-const zip = $('#buyerzip').val();
-const city = $('#buyercity').val();
-const country = $('#buyercountry').children("option:selected").attr('id');
+console.log('Loaded')
 
-let big_table = $('#ERSShipnHand').find("table");
+const texts = $('.info-item dd .txt-tobe-copied').toArray().map((e)=>{
+    return $(e).text()
+})
+
+const name = texts[3];
+const address1 = texts[4];
+const address2 = "";
+const zip = texts[5];
+const city = texts[6];
+const country = texts[7];
+
 
 let all_articles = []
 
-const articles = big_table.find('tbody tr').get().filter((_, i) => i > 0).map((e) => {
-    return $(e).find('td').get().filter((e, i) => i !== 2 && i < 5).map(e => $.trim($(e).text()));
-});
-let shipping = parseFloat($('#ShippingAndHandling1').val());
+// const articles = big_table.find('tbody tr').get().filter((_, i) => i > 0).map((e) => {
+//     return $(e).find('td').get().filter((e, i) => i !== 2 && i < 5).map(e => $.trim($(e).text()));
+// });
+const articles = $('.items-row').toArray().map((e)=>{
+    return [
+        parseInt($(e).find('.quantity').text()),
+        $(e).find('.item-itemID').text().replace('(', '').replace(')', ''),
+        $(e).find('.item-title').text(),
+        $(e).find('.soldFor').text().replace('€', '')
+    ]
+})
+let shipping = parseFloat($('.totals-summary > dl > div:nth-child(2) > dd').text().replace('€', ''));
 console.log(articles, shipping);
 console.log(name, address1, address2, zip, city, country);
-console.log(big_table);
 
-let dates = $('table.shreskin-edit-sales-record-status-summary').find('tbody tr').get().filter((_, i) => i === 1)[0]
+all_events = $('.purchase-details .info-item dd').toArray()
+let buyDay = $(all_events[3]).text()
+let payDay = $(all_events[4]).text().split(' ').slice(0, 3).join(" ")
 
-let all_events = $(dates).find("td[width='200'] b, td[width='200'] a").get();
-let buyDay = $(all_events[0]).text()
-let payDay = $(all_events[1]).text()
+const changeDate = d => {
+    const parts = d.split(' ')
+    parts[1] += '.'
+    return parts.join(" ")
+}
 
 console.log(buyDay, payDay)
 
 
 moment.locale('de');
-buyDay = moment(buyDay, "DD-MM-YY");
-payDay = moment(payDay, "DD-MM-YY");
+buyDay = moment(changeDate(buyDay), "ll");
+payDay = moment(changeDate(payDay), "ll");
 let buyDateString = buyDay.format('LL');
 let invoiceFileDate = buyDay.format('YYYYMMDD');
 let payDateString = payDay.format('LL');
